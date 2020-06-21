445cab03883ad26f1609cfbcabf8d84fba4d4435
AyubIRZ <ayubirazeh@gmail.com>
2020-06-21 18:01:21 +0430

    update Employee model

--- app/app/Employee.php
+++ app/app/Employee.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+      'name',
+      'priority',
+      'is_ready',
+    ];
+
+    /**
+     * Relationship with the Calls.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function calls()
+    {
+        return $this->hasMany(Call::class);
+    }
 }
