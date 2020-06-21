04fb7f4423d23c984c691a206ef027bd296032c3
AyubIRZ <ayubirazeh@gmail.com>
2020-06-21 20:46:20 +0430

    add controller method for making call endpoint

--- app/app/Http/Controllers/API/CallController.php
+++ app/app/Http/Controllers/API/CallController.php
@@ -1,10 +1,53 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MakeCallRequest;
+use App\Repositories\CallRepositoryInterface;
 
 class CallController extends Controller
 {
-    //
+    /**
+     * Local repository instance.
+     *
+     * @var CallRepositoryInterface
+     */
+    private $call;
+
+    /**
+     * CallController constructor.
+     * @param CallRepositoryInterface $call
+     */
+    public function __construct(CallRepositoryInterface $call)
+    {
+        $this->call = $call;
+    }
+
+    /**
+     * Makes a new incoming call and assigns it to a call center employee.
+     *
+     * @param MakeCallRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function make(MakeCallRequest $request)
+    {
+        $call = $this->call->create($request->all());
+
+        if ($call->employee_id == null) {
+            $response = [
+                'ok' => true,
+                'message' => 'A new call has been made and added to the call center queue!',
+                'data' => $call
+            ];
+        } else {
+            $response = [
+                'ok' => true,
+                'message' => 'A new call has been made and assigned to an employee!',
+                'data' => $call
+            ];
+        }
+
+        return response()->json($response, 201);
+    }
 }
