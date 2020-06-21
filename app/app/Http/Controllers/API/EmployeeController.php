c3c35240897a115c2547cf4ed5399b5bce216ead
AyubIRZ <ayubirazeh@gmail.com>
2020-06-21 19:05:24 +0430

    add controller method for Show Employee endpoint

--- app/app/Http/Controllers/API/EmployeeController.php
+++ app/app/Http/Controllers/API/EmployeeController.php
@@ -1,10 +1,45 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
+use App\Repositories\EmployeeRepositoryInterface;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    /**
+     * Local repository instance.
+     *
+     * @var EmployeeRepositoryInterface
+     */
+    private $employee;
+
+    /**
+     * EmployeeController constructor.
+     * @param EmployeeRepositoryInterface $employee
+     */
+    public function __construct(EmployeeRepositoryInterface $employee)
+    {
+        $this->employee = $employee;
+    }
+
+    public function show($employee)
+    {
+        $employee = $this->employee->find($employee);
+
+        if ($employee == null) {
+            return response()->json([
+                'ok' => false,
+                'message' => 'The requested employees was not found!',
+                'data' => $employee,
+            ]);
+        }
+
+        return response()->json([
+            'ok' => true,
+            'message' => 'The requested employees was found!',
+            'data' => $employee,
+        ]);
+    }
 }
