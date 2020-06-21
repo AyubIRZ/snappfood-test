051b723cf7a699a99344807cb9bb5bab9f31eb8a
AyubIRZ <ayubirazeh@gmail.com>
2020-06-21 18:01:50 +0430

    update Call model

--- app/app/Call.php
+++ app/app/Call.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Call extends Model
 {
-    //
+    protected $fillable = [
+        'is_instant',
+        'employee_id',
+        'is_closed',
+    ];
+
+    /**
+     * Relationship with the Employees.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
